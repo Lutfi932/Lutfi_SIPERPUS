b3aa6c9160191a64461f251a47af080eafa1879d
SUSANPUTRIANGGRAENI <susanputrianggraeni@gmail.com>
2024-11-09 11:18:12 +0700

    Add : Spatie in laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,16 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        
 
         $this->call([
-            BookshelfSeeder::class,
-            BookSeeder::class
+            // BookshelfSeeder::class,
+            // BookSeeder::class,
+            // RoleSeeder::class,
+            // PermissionSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +16,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get("/books", [BookController::class, "index"])->name("books.index");
+
+});
+Route::group(['middleware' => ['role:pustakawan']], function () {
+    Route::get("/books/create", [BookController::class, "create"])->name("books.create");
+    Route::post("/books/store", [BookController::class, "store"])->name("books.store");
+    Route::get("/books/edit/{id}", [BookController::class, "edit"])->name("books.edit");
+    Route::patch("/books/update", [BookController::class, "update"])->name("books.update");
+
 });
 
 require __DIR__.'/auth.php';
